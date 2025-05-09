467ee3806b8348905490a68cce6fc432264a5148
minhyuk <martingodfamily@gmail.com>
2025-05-09 20:03:52 +0900

    타이머 구현

---
+++ src/TimerSwitch.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import Timer from "./Timer";
+
+const TimerSwitch = () => {
+    const [showTimer, setShowTimer] = useState(true);
+    const toggleTimer = () => {
+        setShowTimer((prev) => !prev);
+    };
+
+  return (
+    <div>
+        <h1>타이머 예제</h1>
+        <button onClick={toggleTimer} className="border">
+            {showTimer ? "타이머숨기기" : "타이머보이기"}
+        </button>
+        <hr/>
+        {showTimer&& <Timer />}
+    </div>
+  );
+  
+};
+
+export default TimerSwitch
